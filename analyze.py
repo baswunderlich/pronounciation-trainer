021e45f3ee27cd0a5c0bedfac53bd16da5839402
Adrian Stangl <stangl.adrian@web.de>
2025-02-12 14:43:09 +0100

    improve plot

--- analyze.py
+++ analyze.py
@@ -3,43 +3,57 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
+# Open WAV file
 wav_obj = wave.open(sys.argv[1], 'rb')
 
-sample_freq = wav_obj.getframerate()
-print(str(sample_freq))
+# Get basic properties
+sample_freq = wav_obj.getframerate()  # Sample rate
+n_samples = wav_obj.getnframes()  # Total number of frames
+t_audio = n_samples / sample_freq  # Duration in seconds
+n_channels = wav_obj.getnchannels()  # Mono (1) or Stereo (2)
 
-n_samples = wav_obj.getnframes()
-print(str(n_samples))
+print(f"Sample Rate: {sample_freq} Hz")
+print(f"Number of Samples: {n_samples}")
+print(f"Duration: {t_audio:.2f} seconds")
+print(f"Channels: {n_channels}")
 
-t_audio = n_samples/sample_freq
-print(str(t_audio))
-
-n_channels = wav_obj.getnchannels()
-print(str(n_channels))
-
+# Read audio data
 signal_wave = wav_obj.readframes(n_samples)
 signal_array = np.frombuffer(signal_wave, dtype=np.int16)
 
-l_channel = signal_array[0::2]
-r_channel = signal_array[1::2]
+# Stereo handling: separate left & right channels
+if n_channels == 2:
+    l_channel = signal_array[0::2]
+    r_channel = signal_array[1::2]
+    signal_array = l_channel  # Default to left channel for visualization
+else:
+    l_channel = signal_array
+    r_channel = None  # No right channel in mono
 
-times = np.linspace(0, n_samples/sample_freq, num=n_samples)
-print("times", str(len(times)))
-print("len(l_channel)", str(len(l_channel)))
+# Time axis correction
+times = np.linspace(0, t_audio, num=len(l_channel))
 
-plt.figure(figsize=(15, 5))
-plt.plot(times, signal_array)
-plt.title('Left Channel')
-plt.ylabel('Signal Value')
-plt.xlabel('Time (s)')
-plt.xlim(0, t_audio)
-plt.show()
+# --- Create a single figure with 2 subplots (2 rows, 1 column) ---
+fig, axes = plt.subplots(2, 1, figsize=(9, 6))  # 2 rows, 1 column
 
-plt.figure(figsize=(15, 5))
-plt.specgram(signal_array, Fs=sample_freq, vmin=-20, vmax=50)
-plt.title('Left Channel')
-plt.ylabel('Frequency (Hz)')
-plt.xlabel('Time (s)')
-plt.xlim(0, t_audio)
-plt.colorbar()
+# --- Plot Waveform in First Subplot ---
+axes[0].plot(times, l_channel, label="Left Channel", color="blue")
+if r_channel is not None:
+    axes[0].plot(times, r_channel, label="Right Channel", color="red", alpha=0.7)
+axes[0].set_title('Waveform')
+axes[0].set_ylabel('Signal Value')
+axes[0].set_xlabel('Time (s)')
+axes[0].set_xlim(0, t_audio)
+axes[0].legend()
+
+# --- Plot Spectrogram in Second Subplot ---
+axes[1].specgram(l_channel, Fs=sample_freq, vmin=-20, vmax=50)
+axes[1].set_title('Spectrogram (Left Channel)')
+axes[1].set_ylabel('Frequency (Hz)')
+axes[1].set_xlabel('Time (s)')
+axes[1].set_xlim(0, t_audio)
+fig.colorbar(plt.cm.ScalarMappable(), ax=axes[1], label="Intensity (dB)")  # Colorbar for spectrogram
+
+# --- Adjust layout and show figure ---
+plt.tight_layout()
 plt.show()
